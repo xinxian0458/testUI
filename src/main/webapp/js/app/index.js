3bccd290298297f5db48a1bb47a5826a2bb78061
Zhang Jie <jie.zhang2@hp.com>
2013-10-17 18:06:35 +0800

    api for catalog

--- src/main/webapp/js/app/index.js
+++ src/main/webapp/js/app/index.js
@@ -1,26 +1,18 @@
 angular.module('indexApp', [ 'ngCookies', 'ngResource' ]);
-var CatalogListCtrl = function($scope, $http, $cookies, $resource) {
+var CatalogListCtrl = function($scope, $cookies, $resource) {
 	$scope.catalog_list = [];
-	// $http.get('http://16.158.166.166:8080/HPCC/tenants/getTenantsForRegister')
-	// .success(function(response, status, headers, config) {
-	// $scope.catalog_list = response;
-	// });
-	var Catalog = $resource('http://16.158.166.166\\:8080/HPCC/tenants/getTenantsForRegister/:catalogId');
-	$scope.catalog_list = Catalog.query({
-		format : "JSON"
+	var Catalog = $resource('/csa/rest/catalog/:catalogId', {}, {
+		'query' : {
+			method : "GET",
+			transformResponse : function(data, headersGetter) {
+				return angular.fromJson(data).catalog;
+			},
+			isArray : true
+		}
 	});
-	// var Catalog = $resource('/csa/rest/catalog/:catalogId', {}, {
-	// 'query' : {
-	// method : "GET",
-	// transformResponse : function(data, headersGetter) {
-	// return angular.fromJson(data).catalog;
-	// },
-	// isArray : true
-	// }
-	// });
-	// $scope.catalog_list = Catalog.query({
-	// userIdentifier : $cookies.token,
-	// scope : "base",
-	// detail : "basic"
-	// });
+	$scope.catalog_list = Catalog.query({
+		userIdentifier : $cookies.token,
+		scope : "base",
+		detail : "basic"
+	});
 }
